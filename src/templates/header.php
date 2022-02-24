b7ec9c79c73a2f0a8f41cf6f19ea8f8cb78f0ffd
VANESSA LIZBETH RONQUILLO SANTOS <vanessa.ronquillos@ug.edu.ec>
2022-02-24 17:38:31 -0500

    links headers

--- src/templates/header.php
+++ src/templates/header.php
@@ -1,6 +1,6 @@
     <header class="bg-black p-2">
         <div class="container flex items-center flex-col sm:flex-row justify-between mx-auto text-white">
-            <a href="BayasMarcos.html" class="flex items-center">
+            <a href="../BayasCrud/presentar.php" class="flex items-center">
                 <h1 class="my-2 text-4xl sm:ml-0 sm:text-title sm:leading-none ">EcuTravel</h1>
                 <i class="fas fa-plane-departure text-2xl sm:text-logo ml-2"></i>
             </a>
@@ -11,7 +11,7 @@
                 class="opacity-0 h-0 invisible sm:visible sm:h-auto sm:opacity-100 sm:flex gap-2 flex-col text-center sm:flex-row sm:justify-between sm:gap-4 sm:text-2sm transition-all duration-300 ease-in-out">
                 <a class="hover:text-orange-500 text-lg sm:text-base" href="RuizJonathan.html">Vuelos</a>
                 <a class="hover:text-orange-500 text-lg sm:text-base" href="DavilaJose.html">Hoteles</a>
-                <a class="hover:text-orange-500 text-lg sm:text-base" href="RonquilloVanessa.html">Reserva de Autos</a>
+                <a class="hover:text-orange-500 text-lg sm:text-base" href="../renta_autos/presentar.php">Reserva de Autos</a>
                 <a class="hover:text-orange-500 text-lg sm:text-base" href="TenemeaNeysser.html">Gastronomia</a>
             </nav>
         </div>
