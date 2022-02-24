2b7fbcea82eb057c0d937c39b0fed3b94f8f1992
MarcosBayas95 <marcos.bayase@ug.edu.ec>
2022-02-24 15:58:12 -0500

    Tarea2
    BayasCrud

---
+++ src/BayasCrud/Agregar.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="../styles/PrincipalEstilos.css"/>
+    <link rel="stylesheet" href="../styles/style.css">
+    <title>Formulario de Reserva de Viajes</title>
+    
+</head>
+<body>
+    <div id="contenedor">
+        <header class="bg-black p-2">
+            <div class="container flex items-center flex-col sm:flex-row justify-between mx-auto text-white">
+                <a href="../index.html" class="flex items-center">
+                    <h1 class="my-2 text-4xl sm:ml-0 sm:text-title sm:leading-none ">EcuTravel</h1>
+                    <i class="fas fa-plane-departure text-2xl sm:text-logo ml-2"></i>
+                </a>
+    
+                <i id="bar" class="block text-3xl sm:hidden fas fa-bars"></i>
+    
+                <nav id="nav" class="opacity-0 h-0 invisible sm:visible sm:h-auto sm:opacity-100 sm:flex gap-2 flex-col text-center sm:flex-row sm:justify-between sm:gap-4 sm:text-2sm transition-all duration-300 ease-in-out">
+                    <a class="hover:text-orange-500 text-lg sm:text-base" href="../RuizJonathan.html">Vuelos</a>
+                    <a class="hover:text-orange-500 text-lg sm:text-base" href="../DavilaJose.html">Hoteles</a>
+                    <a class="hover:text-orange-500 text-lg sm:text-base" href="../RonquilloVanessa.html">Reserva de Autos</a>
+                    <a class="hover:text-orange-500 text-lg sm:text-base" href="../TenemeaNeysser.html">Gastronomia</a>
+                </nav>
+            </div>
+        </header>
+            
+    </div><br>
+    <div class="reservas">
+        <nav>
+            <a href="presentar.php">Reservaciones</a>
+        </nav>
+    </div>
+    <section class="formu" id="formul">
+        <div class="formulario">
+            <h3>RESERVA TU VIAJE AHORA</h3>
+            <!-- Formulario -->
+            <form method="post" id="lario">
+            <div class="field">
+                <label for="id">ID</label>
+                <input  type="number" name="id"> 
+                <p></p>
+            </div>
+            
+            <div class="field">
+                <label for="usuario">Usuario</label>
+                <input type="text" name="usuario"> 
+                <p></p>
+            </div>
+            <div class="field">
+                <label for="nombre">Nombre y Apellido</label>
+                <input  type="text" name="nombre"> 
+                <p></p>
+            </div>
+            <div class="field">
+                <label for="contraseña">Contraseña</label>
+                <input type="password" name="contraseña"> 
+                <p></p>
+            </div>
+            <div class="field">
+                <label for="lugar">Destino</label>
+                <select name="lugar">
+                    <option selected disabled >Selecione</option>
+                   <option>Quito</option> 
+                   <option>Cuenca</option> 
+                   <option>Baños</option> 
+                   <option>Guayaquil</option> 
+                   <option>Galápagos</option> 
+                </select>
+                <p></p>
+            </div>
+            <div class="field">
+                <label for="fecha">Fecha del viaje</label>
+                <input  type="date" name="fecha"> 
+                <p></p>              
+            </div>
+            <div class="field">
+                <label for="email">E-mail</label>
+                <input  type="email" name="email" placeholder="ej. minombre@gmail.com"> 
+                <p></p>              
+            </div>           
+            <div class="field">
+                <label for="telefono">Teléfono</label>
+                <input type="text" name="telefono" placeholder="09- --- ----"> 
+                <p></p>               
+            </div>
+            <div class="submit">
+                <button>AGREGAR</button>
+            </div>
+        </form> 
+       
+    </section>
+    <?php
+	// incluir archivo conexion.php
+	require_once 'conexion.php';
+	if (!empty($_POST['id']) && !empty($_POST['usuario']) &&
+			 !empty($_POST['nombre']) && !empty($_POST['contraseña']) && !empty($_POST['lugar'])
+			 && !empty($_POST['fecha']) && !empty($_POST['email']) &&
+			 !empty($_POST['telefono'])) {
+               
+
+		$id = htmlentities($_POST['id']);
+		$usuario = htmlentities($_POST['usuario']);
+		$nombre = htmlentities($_POST['nombre']);
+		$contraseña = htmlentities($_POST['contraseña']);
+        $lugar = htmlentities($_POST['lugar']);
+        $fecha = htmlentities($_POST['fecha']);
+		$email = isset($_POST['email']) ? htmlentities($_POST['email']) : '';
+		$telefono = htmlentities($_POST['telefono']);
+
+		$sql = "insert into reserva(id, usuario, nombre, contraseña, destino, fecha_viaje, email, telefono) "
+				. "values($id,'$usuario','$nombre','$contraseña','$lugar','$fecha','$email','$telefono')";
+
+		if (mysqli_query($con, $sql)) {// si se ejecuto sin errores
+			//header("location:presentar.php"); //redireccionar
+           echo '<script>window.location="presentar.php"</script>';
+		} else {
+			echo "Error: " . $sql . "" . mysqli_error($con);
+		}
+	}
+    
+    
+	?>
+    
+    <footer class="bg-black">
+        <div class="container mx-auto flex items-center justify-center flex-col p-2">
+            <a href="/" class="text-white">
+                <i class="text-3xl fas fa-plane-departure"></i>
+            </a>
+            <p class="text-white text-sm">Todos los Derechos Reservados EcuTravel &copy;</p>
+        </div>
+    </footer>
+    <script src="./bundle.js"></script> 
+</body>
+</html>
---
+++ src/BayasCrud/turismo_reserva.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: turismo
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reserva`
+--
+
+DROP TABLE IF EXISTS `reserva`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reserva` (
+  `id` int NOT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `contraseña` varchar(45) DEFAULT NULL,
+  `destino` varchar(45) DEFAULT NULL,
+  `fecha_viaje` date DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `telefono` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reserva`
+--
+
+LOCK TABLES `reserva` WRITE;
+/*!40000 ALTER TABLE `reserva` DISABLE KEYS */;
+INSERT INTO `reserva` VALUES (2,'Joel34','Joel Mero','20478','Quito','2022-04-08','joel@gmail.com','0987456314'),(3,'andres69','Andres Mero','424234','Ba&ntilde;os','2022-04-30','andres@hotmail.com','0987456314'),(5,'May2003','Maybeth Jimenez','12123','Gal&aacute;pagos','2022-05-07','may@gmail.com','0987321544');
+/*!40000 ALTER TABLE `reserva` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-24 15:21:37
