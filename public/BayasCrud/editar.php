1c0512ce1c873feb2a585ed59959a05f48ba1c7c
MarcosBayas95 <marcos.bayase@ug.edu.ec>
2022-02-25 17:05:34 -0500

    CrudBayas
    Reservaciones

--- public/BayasCrud/editar.php
+++ public/BayasCrud/editar.php
@@ -14,26 +14,10 @@
 </head>
 <body>
 
-<div id="contenedor">
-        <header class="bg-black p-2">
-            <div class="container flex items-center flex-col sm:flex-row justify-between mx-auto text-white">
-                <a href="../index.html" class="flex items-center">
-                    <h1 class="my-2 text-4xl sm:ml-0 sm:text-title sm:leading-none ">EcuTravel</h1>
-                    <i class="fas fa-plane-departure text-2xl sm:text-logo ml-2"></i>
-                </a>
-    
-                <i id="bar" class="block text-3xl sm:hidden fas fa-bars"></i>
-    
-                <nav id="nav" class="opacity-0 h-0 invisible sm:visible sm:h-auto sm:opacity-100 sm:flex gap-2 flex-col text-center sm:flex-row sm:justify-between sm:gap-4 sm:text-2sm transition-all duration-300 ease-in-out">
-                    <a class="hover:text-orange-500 text-lg sm:text-base" href="../RuizJonathan.html">Vuelos</a>
-                    <a class="hover:text-orange-500 text-lg sm:text-base" href="../DavilaJose.html">Hoteles</a>
-                    <a class="hover:text-orange-500 text-lg sm:text-base" href="../RonquilloVanessa.html">Reserva de Autos</a>
-                    <a class="hover:text-orange-500 text-lg sm:text-base" href="../TenemeaNeysser.html">Gastronomia</a>
-                </nav>
-            </div>
-        </header>
-            
-    </div><br>
+<?php
+    include '../templates/header.php';
+?>
+<br>
     <div class="reservas">
         <nav>
             <a href="presentar.php">Reservaciones</a>
@@ -43,13 +27,13 @@
     <?php
 
         // incluir archivo conexion.php
-        require_once 'conexion.php';
+        require_once '../conexion/db.php';
      
 if (isset($_GET['id'])) {
     $id = htmlentities($_GET['id']);
 
     $sql = "select * from reserva where id = '" . $id . "'";
-    $resultado = mysqli_query($con, $sql);
+    $resultado = mysqli_query(conectarDB(), $sql);
     while ($fila = mysqli_fetch_assoc($resultado)) {
         ?>
         <section class="formu" id="formul">
@@ -122,23 +106,19 @@ if (isset($_GET['id'])) {
             $sql2 = "update reserva set id =$idp, usuario ='$usuario', nombre = '$nombre', contraseña = '$contraseña',"
                     . " destino = '$lugar', fecha_viaje = '$fecha', email = '$email', telefono=  '$telefono'  where id=$idp";
 
-            if (mysqli_query($con, $sql2)) {
+            if (mysqli_query(conectarDB(), $sql2)) {
                 // echo "Registro ingresado correctamente";
                 //header("location:presentar.php"); //redireccionar
                 echo '<script>window.location="presentar.php"</script>';
             } else {
-                echo "Error: " . $sql2 . "" . mysqli_error($con);
+                echo "Error: " . $sql2 . "" . mysqli_error(conectarDB());
             }
         }
+
     ?>
-<footer class="bg-black">
-        <div class="container mx-auto flex items-center justify-center flex-col p-2">
-            <a href="/" class="text-white">
-                <i class="text-3xl fas fa-plane-departure"></i>
-            </a>
-            <p class="text-white text-sm">Todos los Derechos Reservados EcuTravel &copy;</p>
-        </div>
-    </footer>
-    <script src="./bundle.js"></script> 
+    <?php
+        include '../templates/footer.php';
+    ?>
+
 </body>
 </html>
